0f725c762c218f313755fbd7ca1bb4cd1844fbf2
mchaewon <m6011703@gmail.com>
2024-04-23 21:46:21 +0900

    estimation modify

---
+++ openpose/pose_estimation.py
@@ -0,0 +1,32 @@
+import cv2
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+
+from src import model
+from src import util
+from src.body import Body
+from src.hand import Hand
+
+import os
+
+
+def main():
+    #crop data 불러오기
+    body_estimation = Body('openpose/model/body_pose_model.pth')
+
+    mode='train'
+    root_path = f'./data/{mode}/crop/'
+    folder_list=os.listdir(root_path)
+    for folder_name in folder_list:
+        img_list=os.listdir(os.path.join(root_path,folder_name))
+        for img in img_list:
+            img_path=os.path.join(root_path, folder_name, img)
+            oriImg=cv2.imread(img_path)
+            candidate, subset = body_estimation(oriImg)
+            canvas = copy.deepcopy(oriImg)  
+            canvas = util.draw_bodypose(canvas, candidate, subset)
+
+#crop을 들고와야함. crop이 test_image여야함. 
+if __name__ == "__main__":
+    main()
