da610095e152b1d08865b2f5f568acd5102d0cab
EunjiKim02 <now0104@naver.com>
2024-05-19 23:30:18 +0900

    DB connect & change fastapi to flask

--- fall_detection_system/app.py
+++ fall_detection_system/app.py
@@ -1,12 +1,8 @@
-from fastapi import FastAPI
-from fall_detection_system.routes.main_routes import main_routers
-from fall_detection_system.routes.monitor_routes import monitor_routes
-
-# router를 Fast API에 연결
+from flask import Flask
+from fall_detection_system.routes.main_routes import main_bp
 
 def create_app():
-    app = FastAPI()
-    app.include_router(main_routers, prefix='')
-    app.include_router(monitor_routes)
+    app = Flask(__name__)
+    app.register_blueprint(main_bp, url_prefix='/')
 
     return app
