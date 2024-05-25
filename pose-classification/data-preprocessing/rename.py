aa1b4b5e3739989aa1709300d4c1aaf510177dc2
jaehyeonha <mini0950@knu.ac.kr>
2024-05-25 21:53:07 +0900

    delete compile

--- pose-classification/data-preprocessing/rename.py
+++ pose-classification/data-preprocessing/rename.py
@@ -13,13 +13,10 @@ high_i의 형태를 가진 이름으로 변경
 source_path = "./source"
 
 for high in os.listdir(source_path):
-    print(high)
     i = 0
     for folder in os.listdir(os.path.join(source_path, high)):
-        print(folder)
         for img in os.listdir(os.path.join(source_path, high, folder)):
             img_path = os.path.join(source_path, high, folder, img)
-            print(img_path)
             os.rename(
                 img_path, os.path.join(source_path, high, folder, f"{high}_{i}.jpg")
             )
