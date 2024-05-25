40d66d6b61e491bc4510cbd5815f6e4382c28c2c
jaehyeonha <mini0950@knu.ac.kr>
2024-05-25 21:52:45 +0900

    add annotation

--- pose-classification/data-preprocessing/rename.py
+++ pose-classification/data-preprocessing/rename.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 
+"""
+용도 : 파일 이름 변경
+
+코드실행 결과 : ./source/high/folder의 이미지를 이용하여
+high_i의 형태를 가진 이름으로 변경
+
+"""
+
 source_path = "./source"
 
 for high in os.listdir(source_path):
