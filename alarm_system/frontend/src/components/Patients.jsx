246f08f3329fc93bc5b658c82d3f05bfa0ba254d
EunjiKim02 <now0104@naver.com>
2024-06-03 23:02:05 +0900

    ADD : function closing warning window connects to DB

--- alarm_system/frontend/src/components/Patients.jsx
+++ alarm_system/frontend/src/components/Patients.jsx
@@ -10,13 +10,16 @@ export default function Patients() {
   const [isWarning, setIsWarning] = useState(false);
   const [warningInfo, setWarningInfo] = useState({warningText: "경고", detail: "환자가 위험합니다."});
   const [patients, setPatients] = useState([]);
-
+  const [warningPatient, setwarningPatient] = useState(0);
+  const [socket, setSocket] = useState(io("http://localhost:5000"));
 
   const fetchAPI = async () => {
     try {
       const response = await axios.get("http://localhost:5000/patients");
       console.log(response.data);
       setPatients(response.data.patients);
+      const socket = io("http://localhost:5000");
+      setSocket(socket)
     } catch (error) {
       console.error('Error fetching patients:', error);
     }
@@ -27,11 +30,11 @@ export default function Patients() {
   }, []);
 
   useEffect(() => {
-    const socket = io("http://localhost:5000");
 
     socket.on('warning', (data) => {
       console.log('Warning received:', data);
       setWarningInfo({ warningText: "경고", detail: `환자 ID ${data.patient_id}: ${data.message}` });
+      setwarningPatient(data.patient_id)
       setIsWarning(true);
     });
 
@@ -42,6 +45,11 @@ export default function Patients() {
 
   const handleClose = () => {
     setIsWarning(false);
+    console.log(socket)
+    if (socket) {
+      console.log('warning_close')
+      socket.emit('warning_close', { message: warningPatient });
+    }
   };
 
   const renderPatients = patients.length > 0 && patients.map((p) => {
--- alarm_system/server.py
+++ alarm_system/server.py
@@ -6,8 +6,11 @@ from backend.routes.alarm_routes import monitor_patients
 app, socketio = create_app()
 app.secret_key = SECRET_KEY
 
+
 if __name__ == '__main__':
     
     threading.Thread(target=monitor_patients, args=(socketio,)).start()
     #socketio.run(app, debug=True)
     socketio.run(app)
+
+
