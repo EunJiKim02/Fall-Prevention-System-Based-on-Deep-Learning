3cad8cd92ec2c8e8c241074a9cefc39abe532872
jaehyeonha <mini0950@knu.ac.kr>
2024-06-10 23:09:48 +0900

    split commit

--- pose_classification/data_preprocessing/split.py
+++ pose_classification/data_preprocessing/split.py
@@ -2,6 +2,13 @@ import os
 import random
 import shutil
 
+"""
+용도 : train, test 데이터 분류
+
+코드실행 결과 : ./source/high/folder의 이미지를 이용하여 ./train/src/high/folder 폴더와 
+    ./test/high/folder 폴더에 분류
+"""
+
 # data split
 source_path = "./source"
 train_path = "./train/src"
